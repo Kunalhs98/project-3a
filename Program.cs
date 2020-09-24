318c343623516cc3470d44fd4eb6f2a099f6735e
Kunal Samtani <kunalsamtani@usf.edu>
2020-09-24 11:11:35 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Author: Kunal Samtani
+    Due Date: 24/09/2020
+    Comments: Program asks users to enter their name and then gives an output greeting them. 
+*/
+using System;
+
+namespace project_3a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name.");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
